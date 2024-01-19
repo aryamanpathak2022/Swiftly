b004b0b488d320b9f5d00c3b8e3cc44882b220e5
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-01-19 23:13:20 +0530

    test

--- frontend/src/components/FeaturedItems/FeaturedItems.jsx
+++ frontend/src/components/FeaturedItems/FeaturedItems.jsx
@@ -1,61 +1,55 @@
 import React from "react";
-import Slider from "react-slick";
 import "./FeaturedItems.css";
-// Import Slick carousel styles
 
-// Import Slick carousel styles
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+const FeaturedItems = () => {
+  const shiftCards = (direction) => {
+    const carouselItems = document.querySelectorAll('.card');
+    const elems = Array.from(carouselItems);
 
-function FeaturedItems() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-       
-      };
+    if (direction === 'left') {
+      const lastElement = elems.pop();
+      elems.unshift(lastElement);
+    } else if (direction === 'right') {
+      const firstElement = elems.shift();
+      elems.push(firstElement);
+    }
+
+    elems.forEach((element, index) => {
+      const currentClass = element.classList[1];
+      const currentNumber = parseInt(currentClass.split('_')[1]);
+      const newClass = `card_${(currentNumber % 6) + 1}`;
+
+      element.classList.remove(currentClass);
+      element.classList.add(newClass);
+    });
+  };
+
   return (
-    <div className="h-[500px] relative bg-blue">
-      <div className="absolute title gtop-0 left-1/2 transform -translate-x-1/2 p-4 text-white text-5xl">
-        Featured Items
+    <>
+      <div className="card_carousel flex justify-center">
+        <button onClick={() => shiftCards('left')}>Left</button>
+        <div className="card card_1" key="1">
+          1
+        </div>
+        <div className="card card_2" key="2">
+          2
+        </div>
+        <div className="card card_3" key="3">
+          3
+        </div>
+        <div className="card card_4" key="4">
+          4
+        </div>
+        <div className="card card_5" key="5">
+          5
+        </div>
+        <div className="card card_6" key="6">
+          6
+        </div>
+        <button className="z:20" onClick={() => shiftCards('right')}>Right</button>
       </div>
-
-      <div className=" absolute top-40 w-full  ">
-        
-        <Slider {...settings}>
-          {/* Example carousel cards, you can replace with your own data */}
-          <div className="bg-orange border h-80   ">
-          
-            <div className="text-white text-center  mt-2">Item 1</div>
-          </div>
-          <div className="bg-orange h-80 ">
-            
-            <div className="text-white text-center mt-2 item">Item 2</div>
-          </div>
-          <div className="bg-orange h-80">
-            
-            <div className="text-white text-center mt-2">Item 4</div>
-          </div>
-          <div className="bg-orange h-80">
-            
-            <div className="text-white text-center mt-2">Item 5</div>
-          </div>
-          <div className="bg-orange h-80">
-            
-            <div className="text-white text-center mt-2">Item 6</div>
-          </div>
-          <div className="bg-orange h-80">
-            
-            <div className="text-white text-center mt-2">Item 7</div>
-          </div>
-          
-          {/* Add more carousel cards as needed */}
-        </Slider>
-      </div>
-    </div>
+    </>
   );
-}
+};
 
 export default FeaturedItems;
